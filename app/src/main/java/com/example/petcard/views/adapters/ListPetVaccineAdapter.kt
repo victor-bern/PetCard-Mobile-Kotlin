876e8042f45bea1ca04d51d35a658223df460800
Victor Bernardes <dartinjs@gmail.com>
2022-07-16 13:38:55 -0300

    creating adapter and viewholder to list of vaccines

---
+++ app/src/main/java/com/example/petcard/views/adapters/ListPetVaccineAdapter.kt
@@ -0,0 +1,19 @@
+package com.example.petcard.views.adapters
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.petcard.views.viewholders.ListPetVaccinesViewHolder
+
+class ListPetVaccineAdapter : RecyclerView.Adapter<ListPetVaccinesViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListPetVaccinesViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ListPetVaccinesViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/example/petcard/views/viewholders/ListPetVaccinesViewHolder.kt
@@ -0,0 +1,6 @@
+package com.example.petcard.views.viewholders
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ListPetVaccinesViewHolder(view: View) : RecyclerView.ViewHolder(view)
