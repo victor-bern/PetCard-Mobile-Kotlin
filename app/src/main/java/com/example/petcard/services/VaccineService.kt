7f5413c6b7526b353bbfea1d403269af491cd617
Victor Bernardes <dartinjs@gmail.com>
2022-07-16 18:17:01 -0300

    function to call api and return list of vaccines

--- app/src/main/java/com/example/petcard/services/VaccineService.kt
+++ app/src/main/java/com/example/petcard/services/VaccineService.kt
@@ -1,12 +1,13 @@
 package com.example.petcard.services
 
 import com.example.petcard.models.Vaccine
+import retrofit2.Call
 import retrofit2.http.GET
 
 interface VaccineService {
     @GET("api/v1/vaccines/from-pet/{petId}")
-    fun getVaccinesFromPet(petId: Long): List<Vaccine>
+    fun getVaccinesFromPet(petId: Long): Call<List<Vaccine>>
 
     @GET("api/v1/vaccines/{petId}")
-    fun getVaccine(id: Long): Vaccine?
+    fun getVaccine(id: Long): Call<Vaccine?>
 }
