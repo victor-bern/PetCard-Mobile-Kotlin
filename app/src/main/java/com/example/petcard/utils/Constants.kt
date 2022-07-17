ca18bdc77091b26095b000a1a520856bb0eeb957
Victor Bernardes <dartinjs@gmail.com>
2022-07-17 20:47:26 -0300

    Adding file with Constants

---
+++ app/src/main/java/com/example/petcard/utils/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.petcard.utils
+
+class Constants {
+    companion object {
+        val VACCINES = listOf(
+            "Panleucopenia", "Raiva", "Leucemia",
+            "Rinotraqueite", "Clamidiose", "Calcivirose Sistêmica",
+            "Adenovirose Respiratória", "Parainfluenza", "Bordertolose", "Giadíase", "Leishmaniose Visceral",
+            "Cinomose", "Parvovirose", "Hepatite Infecciosa", "Lepstopirose"
+        )
+    }
+}
